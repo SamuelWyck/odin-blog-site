e1e9dabb663fbd0e90643e5487195c191b0999e2
samuel <samawyck@gmail.com>
2025-07-23 12:28:26 -0500

    Hook up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-// import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from "./router.jsx";
+
+
+
+const router = createBrowserRouter(routes);
+
 
 function App() {
 
   return (
-    <>
-    </>
-  )
-}
+    <RouterProvider router={router} />
+  );
+};
 
-export default App
+
+
+export default App;
