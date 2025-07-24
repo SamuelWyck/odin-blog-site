e986d974fca24bd84dd79249c018cf7c0e18a41b
samuel <samawyck@gmail.com>
2025-07-24 10:35:22 -0500

    Add user menu and finish nav

--- src/components/headerNav.jsx
+++ src/components/headerNav.jsx
@@ -1,25 +1,59 @@
 import "../styles/headerNav.css";
 import arrowImg from "../assets/chevron-down.svg";
+import { useState } from "react";
+import storage from "../utils/storageManager";
 import { Link } from "react-router-dom";
 
 
 
 
 function HeaderNav({user}) {
+    const [rotateImg, setRotateImg] = useState(false);
+    const [hideMenu, setHideMenu] = useState(true);
+
+    
+    document.addEventListener("click", function(event) {
+        if (event.target.matches(".user-menu")) {
+            return;
+        }
+        setRotateImg(false);
+        setHideMenu(true);
+    });
+
+    function handleMenu(event) {
+        event.stopPropagation();
+        setRotateImg(!rotateImg);
+        setHideMenu(!hideMenu);
+    };
+
+
+    function handleLogout(event) {
+        event.stopPropagation();
+        storage.clearCookie();
+    };
+
 
     let navChlidren = null;
     if (!user) {
         navChlidren = <>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign up</Link>
         </>
     } else {
         navChlidren = <>
-            <Link to="/admin/posts">Write</Link>
-            <button>
-                {user.username}
-                <img src={arrowImg} alt="arrow" />
+            {!user.isAdmin || <Link to="/admin/posts">Write</Link>}
+            <button onClick={handleMenu} className="user-menu-btn">
+                <span className="username-span">{user.username}</span>
+                <img 
+                    className={
+                        `menu-arrow ${(rotateImg) ? "rotate" : ""}`
+                    } 
+                    src={arrowImg} alt="arrow" 
+                />
             </button>
+            <div className={`user-menu${(hideMenu) ? " hidden" : ""}`}>
+                <Link reloadDocument to="/" onClick={handleLogout}>Log out</Link>
+            </div>
         </>
     }
 
