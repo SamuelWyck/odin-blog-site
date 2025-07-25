9ec2fbcb1b6d3e40cd72cda051cf9c0e1f792061
samuel <samawyck@gmail.com>
2025-07-25 08:49:30 -0500

    Create nav element to redirect root url to /posts

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,16 @@
 import HomePage from "./components/homePage.jsx";
 import AuthPage from "./components/authPage.jsx";
+import { Navigate } from "react-router-dom";
 
 
 
 const routes = [
     {
         path: "/",
+        element: <Navigate to="/posts" replace />
+    },
+    {
+        path: "/posts",
         element: <HomePage />
     },
     {
