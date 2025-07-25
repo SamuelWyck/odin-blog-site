ab9004168a651dda1ac6b43df571fc3468123dad
samuel <samawyck@gmail.com>
2025-07-25 17:36:49 -0500

    Add metohds to handle comments and rename some methods

--- src/utils/apiManager.js
+++ src/utils/apiManager.js
@@ -62,7 +62,7 @@ class APIManager {
     };
 
 
-    async authPost(reqBody, signup) {
+    async authenticateUser(reqBody, signup) {
         const endPoint = (signup) ? "signup" : "login"
         const url = `${this.#apiDomain}/auth/${endPoint}`;
         const options = {
@@ -71,6 +71,59 @@ class APIManager {
             body: reqBody,
             headers: {
                 "content-type": "application/json"
+            }
+        };
+
+        const response = await this.#makeApiCall(url, options);
+        return response;
+    };
+
+
+    async newComment(reqBody) {
+        const url = `${this.#apiDomain}/comments/new`;
+        const token = this.#storage.getCookie();
+        const options = {
+            mode: "cors",
+            method: "POST",
+            body: reqBody,
+            headers: {
+                "content-type": "application/json",
+                authorization: `Bearer ${token}`
+            }
+        };
+
+        const response = await this.#makeApiCall(url, options);
+        return response;
+    };
+
+
+    async editComment(reqBody, commentId) {
+        const url = `${this.#apiDomain}/comments/edit/${commentId}`;
+        const token = this.#storage.getCookie();
+        const options = {
+            mode: "cors",
+            method: "PUT",
+            body: reqBody,
+            headers: {
+                "content-type": "application/json",
+                authorization: `Bearer ${token}`
+            }
+        };
+
+        const response = await this.#makeApiCall(url, options);
+        return response;
+    };
+
+
+    async deleteComment(commentId) {
+        const url = `${this.#apiDomain}/comments/delete/${commentId}`;
+        const token = this.#storage.getCookie();
+        const options = {
+            mode: "cors",
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+                authorization: `Bearer ${token}`
             }
         };
 
