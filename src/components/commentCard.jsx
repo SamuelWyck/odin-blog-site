c09999a0424879172de554807312b0813e720195
samuel <samawyck@gmail.com>
2025-07-25 17:41:09 -0500

    Add delete logic

--- src/components/commentCard.jsx
+++ src/components/commentCard.jsx
@@ -2,6 +2,7 @@ import "../styles/commentCard.css";
 import readableDate from "../utils/readableDate";
 import { useState } from "react";
 import apiManager from "../utils/apiManager";
+import snakeImg from "../assets/snake.png";
 
 
 
@@ -10,6 +11,7 @@ function CommentCard({comment, user}) {
     const [editPrompt, setEditPrompt] = useState(false);
     const [editing, setEditing] = useState(false);
     const [textValue, setTextValue] = useState(comment.text);
+    const [deleted, setDeleted] = useState(false);
 
 
     function handleDeletePrompt() {
@@ -51,7 +53,7 @@ function CommentCard({comment, user}) {
         };
         reqBody = JSON.stringify(reqBody);
 
-        const res = await apiManager.editCommentPut(
+        const res = await apiManager.editComment(
             reqBody, 
             comment.id
         );
@@ -65,14 +67,30 @@ function CommentCard({comment, user}) {
     };
 
 
+    async function handleDelete() {
+        const res = await apiManager.deleteComment(comment.id);
+
+        if (res.errors) {
+            return;
+        }
+
+        setDeleted(true);
+    };
+
+
+    if (deleted) {
+        return null;
+    }
+
     return (
     <div className="comment-card">
+        <img src={snakeImg} alt="snake" className="post-img" />
         <div className="comment-info">
             <p className="comment-user">
                 @{comment.author.username}
                 <span className="comment-date"> {readableDate(comment.createdAt)}</span>
             </p>
-            {(user.id === comment.authorId) ?
+            {(user && user.id === comment.authorId) ?
                 <div className="comment-btns">
                     <button onClick={handleEditPrompt}>Edit</button>
                     <button onClick={handleDeletePrompt}>
@@ -101,7 +119,7 @@ function CommentCard({comment, user}) {
         {(deletePrompt) ? 
             <div className="prompt-btns">
                 <button onClick={handleDeleteCancel}>Cancel</button>
-                <button>Delete</button>
+                <button onClick={handleDelete}>Delete</button>
             </div> :
             null
         }
