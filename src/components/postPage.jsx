695bca1b075fcd60fd01ab46722b8abfece077cd
samuel <samawyck@gmail.com>
2025-07-25 17:40:23 -0500

    Finish post page

--- src/components/postPage.jsx
+++ src/components/postPage.jsx
@@ -2,10 +2,12 @@ import "../styles/postPage.css";
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
 import CommentForm from "./commentForm.jsx";
+import CommentCard from "./commentCard.jsx";
 import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import apiManager from "../utils/apiManager.js";
 import readableDate from "../utils/readableDate.js";
+import snakeImg from "../assets/snake.png";
 
 
 
@@ -27,10 +29,69 @@ function PostPage() {
                     author: res.post.author.username,
                     replies: res.post.comments.length
                 });
-                setComments(res.post.comments);
+
+                const commentCards = [];
+                for (let comment of res.post.comments) {
+                    commentCards.push(
+                        <CommentCard 
+                            key={comment.id} 
+                            user={res.user} 
+                            comment={comment}
+                        />
+                    );
+                }
+                setComments(commentCards);
             }
         });
     }, [postId]);
+
+
+    function getCommentCards(comments) {
+        const commentCards = [];
+        for (let comment of comments) {
+            commentCards.push(
+                <CommentCard 
+                    user={user} 
+                    key={comment.id} 
+                    comment={comment}
+                />
+            );
+        }
+        return commentCards;
+    };
+
+
+    async function handleCommentSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        let reqBody = {
+            postid: postId
+        };
+        for (let entry of formData.entries()) {
+            const [key, value] = entry;
+            reqBody[key] = value;
+        }
+
+        if (!reqBody.text.trim()) {
+            return;
+        }
+        reqBody = JSON.stringify(reqBody);
+
+        const res = await apiManager.newComment(reqBody);
+
+        if (res.errors) {
+            console.log(res.errors);
+            return;
+        }
+
+        event.target.reset();
+
+        const newPost = await apiManager.getPost(postId);
+        if (newPost.post) {
+            setComments(getCommentCards(newPost.post.comments));
+        }
+    };
 
 
 
@@ -50,23 +111,28 @@ function PostPage() {
     <div className="full-post">
         <div className="post-shadow">
         <div className="post">
-            <p className="full-post-title">{post.title}</p>
-            <p className="full-post-info">
+            <p className="full-post-title">
+                <img src={snakeImg} alt="snake" className="post-img" />
+                {post.title}
+            </p>
+            <div className="full-post-info">
                 <p className="author">@{post.author}</p>
                 <p className="full-post-date">{readableDate(post.date)}</p>
                 <p>Comments ({post.replies})</p>
-            </p>
+            </div>
             <p className="full-post-text">
                 {post.text}
             </p>
         </div>
         </div>
         <p className="comments-title">Comments</p>
-        <div className="comments"></div>
         {(!user) ? 
             <Link to="/login" className="comment-login">Log in to leave a comment</Link> :
-            <CommentForm />
+            <CommentForm handleSubmit={handleCommentSubmit} />
         }
+        <div className="comments">
+            {comments}
+        </div>
     </div>
     <Footer />
     </>
