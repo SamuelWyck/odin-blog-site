da573f1c64fecbbfd16df13a6dc1591ee0a6f0dc
samuel <samawyck@gmail.com>
2025-07-25 12:40:01 -0500

    Factor out date formatting

--- src/components/postCard.jsx
+++ src/components/postCard.jsx
@@ -1,6 +1,6 @@
 import "../styles/postCard.css";
 import { Link } from "react-router-dom";
-import {format} from "date-fns";
+import readableDate from "../utils/readableDate";
 import snakeImg from "../assets/snake.png";
 
 
@@ -12,7 +12,7 @@ function PostCard({post}) {
         <img src={snakeImg} alt="snake" className="post-img" />
         <div className="post-info">
             <p className="post-author">@{post.author.username}</p>
-            <p className="post-date">{format(post.createdAt, "MMM dd yyyy")}</p>
+            <p className="post-date">{readableDate(post.createdAt)}</p>
         </div>
         <p className="post-title">{post.title}</p>
         <p className="post-preview">{post.text.slice(0, 100)}...</p>
