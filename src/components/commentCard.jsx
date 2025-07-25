ab3c9894f1c925a966ac3509615e9e11c396e986
samuel <samawyck@gmail.com>
2025-07-25 16:43:33 -0500

    Create commentCard and add edit functionality

---
+++ src/components/commentCard.jsx
@@ -0,0 +1,121 @@
+import "../styles/commentCard.css";
+import readableDate from "../utils/readableDate";
+import { useState } from "react";
+import apiManager from "../utils/apiManager";
+
+
+
+function CommentCard({comment, user}) {
+    const [deletePrompt, setDeletePrompt] = useState(false);
+    const [editPrompt, setEditPrompt] = useState(false);
+    const [editing, setEditing] = useState(false);
+    const [textValue, setTextValue] = useState(comment.text);
+
+
+    function handleDeletePrompt() {
+        if (!editPrompt) {
+            setDeletePrompt(true);
+        }
+    };
+
+    
+    function handleEditPrompt() {
+        if (!deletePrompt) {
+            setEditPrompt(true);
+            setEditing(true);
+        }
+    };
+
+
+    function handleDeleteCancel() {
+        setDeletePrompt(false);
+    };
+
+
+    function handleEditCancel() {
+        setEditing(false);
+        setEditPrompt(false);
+        setTextValue(comment.text);
+    };
+
+
+    function handleCommentChange(event) {
+        setTextValue(event.target.value);
+    };
+
+
+    async function handleEdit() {
+        let reqBody = {
+            text: textValue,
+            postid: comment.parentId
+        };
+        reqBody = JSON.stringify(reqBody);
+
+        const res = await apiManager.editCommentPut(
+            reqBody, 
+            comment.id
+        );
+
+        if (res.errors) {
+            return;
+        }
+
+        comment.text = textValue;
+        handleEditCancel();
+    };
+
+
+    return (
+    <div className="comment-card">
+        <div className="comment-info">
+            <p className="comment-user">
+                @{comment.author.username}
+                <span className="comment-date"> {readableDate(comment.createdAt)}</span>
+            </p>
+            {(user.id === comment.authorId) ?
+                <div className="comment-btns">
+                    <button onClick={handleEditPrompt}>Edit</button>
+                    <button onClick={handleDeletePrompt}>
+                        Delete
+                    </button>
+                </div> : 
+                null
+            }
+        </div> 
+        {(!editing) ? 
+            <textarea 
+                name="text" 
+                readOnly 
+                className="comment-txt" 
+                value={textValue} 
+            >
+            </textarea> :
+            <textarea 
+                name="text" 
+                className="editing-comment-txt" 
+                onChange={handleCommentChange} 
+                value={textValue}
+            >
+            </textarea>
+        }
+        {(deletePrompt) ? 
+            <div className="prompt-btns">
+                <button onClick={handleDeleteCancel}>Cancel</button>
+                <button>Delete</button>
+            </div> :
+            null
+        }
+        {(editPrompt) ? 
+            <div className="prompt-btns">
+                <button onClick={handleEditCancel}>Cancel</button>
+                <button onClick={handleEdit}>Save</button>
+            </div> :
+            null
+        }
+    </div>
+    );
+};
+
+
+
+export default CommentCard;
