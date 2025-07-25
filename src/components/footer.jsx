7397730bb1a001b9539c5f2bd73a6e1f7e164689
samuel <samawyck@gmail.com>
2025-07-25 11:06:32 -0500

    Create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/footer.css";
+
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; Viper Blog</p>
+        </footer>
+    );
+};
+
+
+
+export default Footer;
