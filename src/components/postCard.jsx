053bc6de5e8d6daaad1f2ecaded579a66b5f520b
samuel <samawyck@gmail.com>
2025-07-28 09:31:03 -0500

    Add published btn and isAdmin prop

--- src/components/postCard.jsx
+++ src/components/postCard.jsx
@@ -2,13 +2,50 @@ import "../styles/postCard.css";
 import { Link } from "react-router-dom";
 import readableDate from "../utils/readableDate";
 import snakeImg from "../assets/snake.png";
+import { useState } from "react";
+import apiManager from "../utils/apiManager";
 
 
 
-function PostCard({post}) {
+function PostCard({post, admin}) {
+    const [published, setPublished] = useState(post.posted);
+
+
+    async function handlePublish() {
+        let reqBody = {
+            title: post.title,
+            text: post.text
+        };
+        if (!published) {
+            reqBody.published = "true";
+        }
+        reqBody = JSON.stringify(reqBody);
+
+        const res = await apiManager.editAuthoredPost(
+            reqBody,
+            post.id
+        );
+
+        if (res.errors) {
+            return;
+        }
+
+        setPublished(!published);
+    };
+
+
     return (
-    <Link className="post-link" to={`/posts/${post.id}`}>
+    <Link 
+        className="post-link" 
+        to={`/${(admin) ? "admin" : ""}/posts/${post.id}`}
+    >
     <article className="post-card">
+        {
+        !admin ||
+        <button className="publish-btn" onClick={handlePublish}>
+            {(published) ? "Unpublish" : "Publish"}
+        </button>
+        }
         <img src={snakeImg} alt="snake" className="post-img" />
         <div className="post-info">
             <p className="post-author">@{post.author.username}</p>
