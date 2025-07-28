b255a96830dec631c2d287178529d162201397e0
samuel <samawyck@gmail.com>
2025-07-28 12:14:37 -0500

    Refactor to class component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,25 +2,53 @@ import "../styles/header.css";
 import snakeImg from "../assets/snake.png";
 import HeaderNav from "./headerNav.jsx";
 import { Link } from "react-router-dom";
+import { Component } from "react";
 
 
 
-function Header({user}) {
-    return (
-    <header>
-        <Link to="/posts">
-        <div className="banner">
-            <img 
-                src={snakeImg} 
-                alt="snake" 
-                className="banner-img" 
-            />
-            <p className="banner-logo">Viper Blog</p>
-        </div>
-        </Link>
-        <HeaderNav user={user} />
-    </header>
-    );
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: null
+        };
+
+        this.updateUser = this.updateUser.bind(this);
+    };
+
+
+    updateUser(user) {
+        if (!this.state.user && !user) {
+            return;
+        }
+        if (this.state.user && user && this.state.user.id === user.id) {
+            return;
+        }
+
+        this.setState(function(state) {
+            return {...state, user: user};
+        });
+    };
+
+
+    render() {
+        return (
+        <header>
+            <Link to="/posts">
+            <div className="banner">
+                <img 
+                    src={snakeImg} 
+                    alt="snake" 
+                    className="banner-img" 
+                />
+                <p className="banner-logo">Viper Blog</p>
+            </div>
+            </Link>
+            <HeaderNav user={this.state.user} />
+        </header>
+        );
+    };
 };
 
 
