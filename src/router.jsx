bf6c564683bea6b1a02d7e196448eebf081e8691
samuel <samawyck@gmail.com>
2025-07-28 12:13:40 -0500

    Restructure router

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import HomePage from "./components/homePage.jsx";
 import AuthPage from "./components/authPage.jsx";
 import PostPage from "./components/postPage.jsx";
 import AdminPage from "./components/adminPage.jsx";
+import RootElement from "./components/rootElement.jsx";
 import { Navigate } from "react-router-dom";
 
 
@@ -9,15 +10,25 @@ import { Navigate } from "react-router-dom";
 const routes = [
     {
         path: "/",
-        element: <Navigate to="/posts" replace />
-    },
-    {
-        path: "/posts",
-        element: <HomePage />
-    },
-    {
-        path: "/posts/:postId",
-        element: <PostPage />
+        element: <RootElement />,
+        children: [
+            {
+                path: "/",
+                element: <Navigate to="/posts" replace />
+            },
+            {
+                path: "/posts",
+                element: <HomePage />
+            },
+            {
+                path: "/posts/:postId",
+                element: <PostPage />
+            },
+            {
+                path: "/admin/posts",
+                element: <AdminPage />
+            }
+        ]
     },
     {
         path: "/login",
@@ -26,10 +37,6 @@ const routes = [
     {
         path: "/signup",
         element: <AuthPage signup={true} />
-    },
-    {
-        path: "/admin/posts",
-        element: <AdminPage />
     }
 ];
 
