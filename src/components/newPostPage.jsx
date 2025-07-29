673f1db9ca3e1760e14aa5fbd7f132673d709ce5
samuel <samawyck@gmail.com>
2025-07-29 13:36:47 -0500

    Factor out post form

--- src/components/newPostPage.jsx
+++ src/components/newPostPage.jsx
@@ -1,18 +1,13 @@
-import "../styles/newPostPage.css";
-import EditorElement from "./EditorElement.jsx";
 import { useState, useEffect } from "react";
-import { useOutletContext, useNavigate } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 import apiManager from "../utils/apiManager.js";
-import ErrorsPartial from "./errorsPartial.jsx";
+import PostForm from "./postForm.jsx";
 
 
 
 function NewPostPage() {
     const {headerRef} = useOutletContext();
-    const navigate = useNavigate();
-    const [txtValue, setTxtValue] = useState("");
     const [apiKey, setApiKey] = useState(null);
-    const [errors, setErrors] = useState(null);
 
     
     useEffect(function() {
@@ -27,72 +22,21 @@ function NewPostPage() {
     }, [headerRef]);
 
 
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-        let reqBody = {
-            text: txtValue
-        };
-        for (let entry of formData.entries()) {
-            const [key, value] = entry;
-            console.log(entry)
-            reqBody[key] = value;
-        }
-        reqBody = JSON.stringify(reqBody);
-
-
-        apiManager.newAuthoredPost(reqBody).then(function(res) {
-            if (res.errors) {
-                setErrors(res.errors);
-                return;
-            }
-
-            navigate("/admin/posts");
-        })
-    };
-
-
-    function editorChange(value) {
-        setTxtValue(value);
-    };
-
-
 
     if (!apiKey) {
         return <p></p>;
     }
 
     return (
-        <div className="post-form-wrapper">
-        <form className="new-post-form" onSubmit={handleSubmit}>
-            <ErrorsPartial errors={errors} />
-            <div className="title-publish-wrapper">
-                <input 
-                    type="text" 
-                    name="title" 
-                    placeholder="Title"
-                    className="new-post-title" 
-                    maxLength={200}
-                    required
-                />
-                <div>
-                    <label htmlFor="published">Publish? </label>
-                    <input type="checkbox" name="published" id="published" />
-                </div>
-            </div>
-            <div className="preview-wrapper">
-                <input 
-                    type="text" 
-                    name="preview" 
-                    placeholder="Preview statement"
-                    required
-                />
-            </div>
-            <EditorElement apiKey={apiKey} initValue={""} handleChange={editorChange} />
-            <button>Create Post</button>
-        </form>
-        </div>
+        <PostForm
+            edit={false}
+            postId={null}
+            editorValue={""}
+            titleValue={""}
+            previewValue={""}
+            published={false}
+            apiKey={apiKey}
+        />
     );
 };
 
