0a07e8ad2d2eb05ba4c4216aca61c8610377449d
samuel <samawyck@gmail.com>
2025-07-29 15:51:09 -0500

    Add pagination logic for posts

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import api from "../utils/apiManager.js";
 import PostCard from "./postCard.jsx";
+import apiManager from "../utils/apiManager.js";
 
 
 
 function HomePage() {
+    const [pageNumber, setPageNumber] = useState(0);
+    const [moreBtn, setMoreBtn] = useState(true);
     const [posts, setPosts] = useState([]);
     const {headerRef} = useOutletContext();
 
     useEffect(function() {
-        api.getPosts().then(function(res) {
+        api.getPosts(pageNumber).then(function(res) {
             if (res.errors) {
                 return;
             }
@@ -19,8 +22,13 @@ function HomePage() {
             const postCards = getPostCards(res.posts);
             headerRef.current.updateUser(res.user);
             setPosts(postCards);
+            if (res.posts.length == apiManager.postPageLength) {
+                setMoreBtn(true);
+            } else {
+                setMoreBtn(false);
+            }
         });
-    }, [headerRef]);
+    }, [headerRef, pageNumber]);
 
 
     function getPostCards(posts) {
@@ -34,12 +42,33 @@ function HomePage() {
     };
 
 
+    function changePageNumber(change) {
+        setPageNumber(p => p + change);
+        window.scrollTo({top: 0, behavior: "smooth"});
+    };
+
+
     return (
-        <div className="posts-wrapper">
-            <div className="posts-list">
-                {posts}
-            </div>
+    <div className="posts-wrapper">
+        <div className="posts-list">
+            {posts}
         </div>
+        <div className="pagination-btns">
+            {(pageNumber > 0) ?
+                <button onClick={() => changePageNumber(-1)}>
+                    Prev Posts
+                </button> :
+                <p></p>
+            }
+            <p></p>
+            {(moreBtn) ?
+                <button onClick={() => changePageNumber(1)}>
+                    More Posts
+                </button> :
+                <p></p>
+            }
+        </div>
+    </div>
     );
 };
 
