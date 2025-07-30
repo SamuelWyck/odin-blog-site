b019fa6554760fd0ad92b324467d744d5db86a59
samuel <samawyck@gmail.com>
2025-07-30 14:33:40 -0500

    Add comment pagination

--- src/components/editPostPage.jsx
+++ src/components/editPostPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import apiManager from "../utils/apiManager";
 import CommentCard from "./commentCard.jsx";
 import PostForm from "./postForm.jsx";
+import PaginationButtons from "./paginationButtons.jsx";
 
 
 
@@ -12,6 +13,8 @@ function EditPostPage() {
     const [post, setPost] = useState(null);
     const [comments, setComments] = useState([]);
     const [apiKey, setApiKey] = useState(null);
+    const [moreBtn, setMoreBtn] = useState(false);
+    const [pageNumber, setPageNumber] = useState(0);
 
 
     useEffect(function() {
@@ -26,24 +29,85 @@ function EditPostPage() {
                 return;
             }
 
-            const comments = [];
-            for (let comment of postRes.post.comments) {
-                comments.push(
-                    <CommentCard
-                        key={comment.id}
-                        comment={comment}
-                        user={postRes.user}
-                        admin={true}
-                    />
-                );
-            }
-
-            setApiKey(apiRes.key);
-            setComments(comments);
-            setPost(postRes.post);
+            const moreBtnStatus = checkForMoreCmts(
+                postRes.post.comments
+            );
             headerRef.current.updateUser(postRes.user);
+            setApiKey(apiRes.key);
+            setComments(getCommentCards(
+                postRes.post.comments, postRes.user
+            ));
+            setPost(postRes.post);
+            setMoreBtn(moreBtnStatus);
         });
     }, [postId, headerRef]);
+
+
+    function checkForMoreCmts(commentList) {
+        let moreBtnStatus = false;
+
+        const numberComments = commentList.length;
+        if (numberComments === apiManager.cmtPageLength) {
+            commentList.pop();
+            moreBtnStatus = true;
+        }
+
+        return moreBtnStatus;
+    };
+
+
+    function getCommentCards(comments, user) {
+        const commentCards = [];
+        for (let comment of comments) {
+            commentCards.push(
+                <CommentCard
+                    key={comment.id}
+                    comment={comment}
+                    user={user}
+                    admin={true}
+                    updateComments={handleCommentDelete}
+                />
+            );
+        }
+        return commentCards;
+    };
+
+
+    async function handleCommentDelete() {
+        const newPageNumber = 0;
+
+        const res = await apiManager.getComments(
+            post.id, newPageNumber
+        );
+        if (res.errors) {
+            return;
+        }
+
+        const moreBtnStatus = checkForMoreCmts(res.comments);
+        setComments(getCommentCards(res.comments, res.user));
+        setPageNumber(newPageNumber);
+        setMoreBtn(moreBtnStatus);
+        headerRef.current.updateUser(res.user);
+    };
+
+
+    async function handlePagination(pageChange) {
+        const newPageNumber = (pageNumber + pageChange >= 0) ?
+        pageNumber + pageChange : 0;
+
+        const res = await apiManager.getComments(
+            post.id, newPageNumber
+        );
+        if (res.errors) {
+            return;
+        }
+
+        const moreBtnStatus = checkForMoreCmts(res.comments);
+        setComments(getCommentCards(res.comments, res.user));
+        setPageNumber(newPageNumber);
+        setMoreBtn(moreBtnStatus);
+        headerRef.current.updateUser(res.user);
+    };
 
 
 
@@ -65,6 +129,12 @@ function EditPostPage() {
         <p className="comments-title">Comments</p>
         <div className="comments">
             {comments}
+            <PaginationButtons 
+                moreBtn={moreBtn}
+                pageNumber={pageNumber}
+                handleClick={handlePagination}
+                comments={true}
+            />
         </div>
         </div>
     ); 
